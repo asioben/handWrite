c6387af15644aa07018429d63f310fde449eb26f
asioben <89880816+asioben@users.noreply.github.com>
2026-06-25 23:47:07 -0400

    impl: dirty conv impl

--- src/cnn.py
+++ src/cnn.py
@@ -17,23 +17,30 @@ class CNN(NeuralNetwork):
         #the dimension of the filter is as such: (filters_size,3,3), it's a 3D array(tensor)
         #here the init
         self.filters = [np.random.randn(filters_size ,3,3) / 9 for i in range(2)]
+        self.conv_biases = [np.zeros((filters_size * i,)) for i in range(1,3)]
         self.max_pool = []
         self.shapes = []
+        self.conv_out = []
+        self.region = []
 
 
     def convolution(self, input):
         h, w, d = input.shape # d for depth again !
         output = np.zeros((h - 2, w - 2, self.filters_size))
-        #print(input.shape)
+        regions = []
         for i in range(h - 2):
             for j in range(w - 2):
                 if d > 1: 
                     region = input[i: (i + 3), j: (j + 3), :]               
-                    output[i, j] = np.vdot(region.T,self.filters[1])
+                    output[i, j] = np.vdot(region.T,self.filters[1]) + self.conv_biases[1]
                 else:
                     input = np.reshape(input,(28,28))
                     region = input[i: (i + 3), j: (j + 3)]
-                    output[i, j] = np.sum(region * self.filters[0], axis = (1,2))
+                    output[i, j] = np.sum(region * self.filters[0], axis = (1,2)) + self.conv_biases[0]
+                regions.append(region)
+
+        self.conv_out.append(output)
+        self.region.append(regions)
                 #output[i, j] = np.sum(region * self.filters, axis = (1,2))
                 #output[i, j] = np.einsum('ijk,ijk',region,self.filters)
 
@@ -92,23 +99,68 @@ class CNN(NeuralNetwork):
         #dO stands for dOutputs
         h, w, _ = dO.shape
 
+        counter = 0
         for i in range(h):
             for j in range(w):
-                dI[self.max_pool[step][(i // h) + j]] = dO[i][j]
+                dI[self.max_pool[step][counter]] = dO[i][j]
+                counter += 1
 
-    def back_propagation(self, y, output):
+        return dI
+
+    #return in this order ->
+    #dI, dW, dB
+    def back_conv(self, dO, step, shape):
+        dB = np.sum(dO,axis=(0,1))
+        dW = np.zeros(self.filters[step].shape)
+        dI = np.zeros(self.shapes[shape])
+        h, w, d = dO.shape
+        h_, w_, d_ = dI.shape
+        #tmp = np.zeros((h,1))
+        
+        counter = 0
+        for i in range(h):
+            for j in range(w):
+                for k in range(d):
+                    if i < (h_ - 2) and j < (w_-2) and k < d_ and step != 0:
+                       dI[i:i+3, j:j+3, k] += dO[i,j,k] * np.rot90(self.filters[step][k],2)
+                    dW += self.region[step][counter].T * dO[i,j,k]
+                counter += 1
+
+        return dI,dW,dB
+
+    def update(self, dW, dB, step):
+        self.filters[step] -= self.learning_rate * dW
+        self.conv_biases[step] -= self.learning_rate * dB
+
+
+    def back_propagation(self, y):
+        #print(self.shapes)
+        self.learning_rate = 0.01
+        #backward
         dO = super().back_propagation(y)
-        self.back_pool(dO,1,-1)
-        #super().update()
-
+        #SGD
+        super().update()
+        #print()
+        shape = -1
+        for i in reversed(range(2)):
+            dO = self.back_pool(dO,i,shape)
+            dO = back_reLU(dO, self.conv_out[i])
+            dO, dW, dB = self.back_conv(dO,i,i)
+            self.update(dW,dB,i)
+            shape -= 2
         
-        
 
+
+#miscellanous functions
+def back_reLU(a, b):   
+   #a represents the tensor that'd get transformed
+   #b represents the mask, the sets of 0 and 1
+   return a * (b > 0)
     
 cnn = CNN(8,(400,64,10))
 input = np.reshape(load.x_train[0],(28,28,1))
 cnn.forward(input)
-cnn.back_propagation(np.reshape(load.y_train[0],(1,1)),None)
+cnn.back_propagation(np.reshape(load.y_train[0],(1,1)))
 """img = load.x_train[0]
 string = "test"
 load.show_img([img,img,img,img,img],[string,string,string,string,string])"""
